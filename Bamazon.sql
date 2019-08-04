db8279abab00027f6e0d8e315be0ded3d9d2e3cd
Greg Manco <gregmanco@gmail.com>
2019-08-04 12:44:19 -0400

    Added code comments to sql file and bamazonCustomer.js

--- Bamazon.sql
+++ Bamazon.sql
@@ -1,23 +1,24 @@
+-- Drops the bamazon_db if it exist currently. --
 DROP DATABASE IF EXISTS bamazon_db;
-
+-- Creates new database called bamazon_db --
 CREATE DATABASE bamazon_db;
-
+-- Iniates use of the bamazon_db
 USE bamazon_db;
-
+-- Creates a table called products
 CREATE TABLE products (
   item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
-  -- Makes a string column called "name" which cannot contain null --
+  -- Makes a string column called "product_name" which cannot contain null --
   product_name VARCHAR(30) NOT NULL,
-  -- Makes a boolean column called "has_pet" which cannot contain null --
+  -- Makes a string  column called "department_name" which cannot contain null --
   department_name VARCHAR(30),
-  -- Makes a sting column called "pet_name" --
+  -- Makes a numeric column called "price" which cannot be null --
   price INTEGER(10) NOT NULL,
-  -- Makes an numeric column called "pet_age" --
+  -- Makes an numeric column called "stock_quantity" --
   stock_quantity INTEGER(10) NOT NULL,
   -- Sets id as this table's primary key which means all data contained within it will be unique --
   PRIMARY KEY (item_id)
 );
-
+-- Inserts values into the products table --
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Bug Assault", "Toys", 40, 10),
 ("Interstellar DVD", "Movies", 20, 20),
@@ -29,7 +30,6 @@ VALUES ("Bug Assault", "Toys", 40, 10),
 ("Holes", "Books", 5, 20),
 ("Outsiders", "Books", 5, 20),
 ("Old School", "Movies", 10, 12);
-
+-- Select ALL from products table. -- 
 SELECT * FROM products;
-3
-SELECT stock_quantity FROM products WHERE item_id = 3;
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-
+// Iniates connection to mysql database
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -22,35 +22,38 @@ connection.connect(function (err) {
     displayTable();
 
 });
-
+// Selects all data from the products database and console logs the table to the terminal
 function displayTable() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.table(res)
-
+        // Iniates the inquirer npm package to query the users responses.
         inquirer
             .prompt([
-                // Here we create a basic text prompt.
+                // Sets up questions for the user on what product they want to buy
                 {
                     type: "input",
                     message: "What is the ID of the Product you want to buy?",
                     name: "product"
                 },
+                // Ask the user of the quantity of items tehy want to buy from the selected item ID
                 {
                     type: "input",
                     message: "What quantity do you want to order?",
                     name: "quantity"
                 }
+                // Selects stock quntity coolumn and price from the selected item id.
             ]).then(function (answers) {
                 connection.query("SELECT stock_quantity, price FROM products WHERE item_id = " + answers.product, function (err, res) {
                     if (err) throw err;
                     console.log(res[0].stock_quantity)
+                    // Setting up variables for specific responses values.
                     var price = res[0].price
                     var quantity = res[0].stock_quantity
-
+                    // If user selects more quantity than currently in stock will respond with "Insufficient Quantitty"
                     if (quantity < answers.quantity) {
                         console.log("Insufficent Quantity")
-
+                        // If user selects an avaliable quantity of the item id they chose and logs the total price of the item * quantity to terminal
                     } else {
                         console.log("Ok you ordered " + answers.quantity + " of that item")
                         quantity = quantity - answers.quantity;
@@ -65,7 +68,6 @@ function displayTable() {
                         })
                     }
 
-                    // console.log(res[0].stock_quantity);
 
                 })
 
@@ -75,5 +77,3 @@ function displayTable() {
 
     });
 }
-//   if ( inquirerResponse.quantity > res.products.stock_quantity)
-//   console.log("")
